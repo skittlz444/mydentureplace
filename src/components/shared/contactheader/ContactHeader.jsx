60e56a3fc9457c349aee8f90a885f82815346c53
Hayden Carson <info@haydencarson.com>
2019-11-14 17:49:57 +1100

    [finishes #169588618]
    Made alot of changes

--- src/components/shared/contactheader/ContactHeader.jsx
+++ src/components/shared/contactheader/ContactHeader.jsx
@@ -1,33 +1,39 @@
 import React from 'react';
-import {Row, Col, Container, Image, Button} from 'react-bootstrap';
+import {Row, Col, Container, Image} from 'react-bootstrap';
 import MediaQuery from 'react-responsive';
 
 import styles from './styles/ContactHeader.module.css';
-import headerImage from './img/My-Denture-Place-Header-50pc-trans.png';
+import headerImage from './img/MDP New Logo.png';
 export default class ContactHeader extends React.Component{
 	render(){
 		return (
 			<React.Fragment>
-				<Container className={styles.headerContainer + ' wow fadeInDown'}>
+				<Container className={styles.headerContainer}>
 					<Container>
 						<Row className="justify-content-center">
-							<Image src={headerImage}/>
-							<MediaQuery minWidth={1200}>
-								<Col className={styles.lgContactInfo}>
-									<h3 className={styles.phone}>
-										<a className={styles.headerAction} href="tel:0741240800">07 4124 0800</a>
-									</h3>
-									<div>
-										Shop 4, 10 Liuzzi St
-									</div>
-									<div>
-										Hervey Bay QLD 4655
-									</div>
-									<div>
-										<a className={styles.headerAction} href="mailto:info@mydentureplace.com.au">info@mydentureplace.com.au</a>
-									</div>
-								</Col>
-							</MediaQuery>
+							<Col>
+							</Col>
+							<Col>
+								<Image src={headerImage} className={styles.headerImage + " wow fadeInDown"}/>
+							</Col>
+							<Col md={{order:1}} className={styles.lgContactInfo + ' justify-content-end wow fadeInDown delay-1s flex'}>
+								<div>
+									<MediaQuery minWidth={800}>
+										<h3 className={styles.phone}>
+											<a className={styles.headerAction} href="tel:0741240800">07 4124 0800</a>
+										</h3>
+										<div>
+											Shop 4, 10 Liuzzi St
+										</div>
+										<div>
+											Hervey Bay QLD 4655
+										</div>
+										<div>
+											<a className={styles.headerAction} href="mailto:info@mydentureplace.com.au">info@mydentureplace.com.au</a>
+										</div>
+									</MediaQuery>
+								</div>
+							</Col>	
 						</Row>
 						</Container>
 				</Container>
