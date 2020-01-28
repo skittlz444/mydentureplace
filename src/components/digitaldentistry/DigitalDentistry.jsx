577455bb533b40d276f2794cbb1e8ea1ccebcef7
Kevdawg1 <kevin151@hotmail.my>
2020-01-28 17:14:05 +0800

    commit

--- src/components/digitaldentistry/DigitalDentistry.jsx
+++ src/components/digitaldentistry/DigitalDentistry.jsx
@@ -4,8 +4,6 @@ import {Row, Col, Container, Button} from 'react-bootstrap';
 
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 
-import image1 from './img/photo 2-1.JPG'
-import image2 from './img/photo 3.JPG'
 import altImage from '@s/loadingimage/loading-image.gif'
 
 export default class DigitalDentistry extends React.Component{
@@ -21,36 +19,21 @@ export default class DigitalDentistry extends React.Component{
 	render(){
 		return (
 			<React.Fragment>
-				<Container className={styles.ourClinicContainer} id="ourclinic">
+				<Container className={styles.digitalDentistryContainer} id="ourclinic">
 					<Container>
 						<Row className="justify-content-center flex">
 							<h2>
-								Our Clinic
+								Digital Dentistry
 							</h2>
 						</Row>
 						<hr/>
 						<Row>
-							<Col lg={4} md={12}>
-							    <LazyLoadImage
-							    src={image1}
-							    alt={altImage}
-							    className={styles.leftImage}
-							     />
-							</Col>
-							<Col lg={5}>
-								<h4>All Health Funds Welcome.</h4>
-								<h4>DVA Provider.</h4>
-								<h4>Latest Technologies.</h4>
-								<h4>Relaxing environment.</h4>
-								<h4>Discreet and personal service.</h4>
-							</Col>
-							<Col lg={3} md={12}>
-							    <LazyLoadImage
-                                src={image2}
-                                alt={altImage}
-                                className={styles.rightImage}
-                                 />
-							</Col>
+						<p className={styles.digiTextSize}>
+						Digital dentures are made using computer-aided design (CAD) and computer-aided manufacturing (CAM) technologies. This innovative process integrates the treatment steps in the dental chair with the digital manufacturing processes in the dental laboratory.
+						</p>
+						</Row>
+						<hr/>
+						<Row>
 						</Row>
 						<hr/>
 						<Row>
